2abd6dab4c92ea90de2e70ded2662b325513cc2d
xschen <xs0040@gmail.com>
2018-02-22 19:57:25 +0800

    implement gtzan genre downloader

--- demo/utility/gtzan_loader.py
+++ demo/utility/gtzan_loader.py
@@ -1,9 +1,9 @@
-from pydl_audio_encoders.library.utility.gtzan_loader import download_gtzan_music_speech
+from pydl_audio_encoders.library.utility.gtzan_loader import download_gtzan_genres
 
 
 def main():
     data_dir_path = '../very_large_data/gtzan'
-    download_gtzan_music_speech(data_dir_path)
+    download_gtzan_genres(data_dir_path)
 
 
 if __name__ == '__main__':
--- pydl_audio_encoders/library/utility/gtzan_loader.py
+++ pydl_audio_encoders/library/utility/gtzan_loader.py
@@ -18,3 +18,17 @@ def download_gtzan_music_speech(data_dir_path):
     tar.extractall(data_dir_path)
     tar.close()
 
+
+def download_gtzan_genres(data_dir_path):
+    zip_file_path = data_dir_path + '/genres.tar.gz'
+
+    if not os.path.exists(zip_file_path):
+        url_link = 'http://opihi.cs.uvic.ca/sound/genres.tar.gz'
+        print('gz model file does not exist, downloading from internet')
+        urllib.request.urlretrieve(url=url_link, filename=zip_file_path,
+                                   reporthook=reporthook)
+
+    tar = tarfile.open(zip_file_path, "r:gz")
+    tar.extractall(data_dir_path)
+    tar.close()
+
